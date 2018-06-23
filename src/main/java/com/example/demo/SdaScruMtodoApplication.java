28169b379bd1f7dfab73b84d3cf847ffa9bf9759
SzymSie <sieklucki.szymon@gmail.com>
2018-06-23 10:04:51 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/SdaScruMtodoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaScruMtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaScruMtodoApplication.class, args);
+	}
+}
